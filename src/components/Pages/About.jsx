7a31bb8f7c5e6a3358db80e81b1645f994630d6f
betiiy-haile <betelhemhaile74@gmail.com>
2023-04-29 22:53:20 +0300

    Splitting pages using routing concept

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,12 @@
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p>A simple github finder App to search users by their username.</p>
+      <p>A project from bradTraversy React course</p>
+    </div>
+  )
+}
+
+export default About
