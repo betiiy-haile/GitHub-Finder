54cc5d7ef9da865eb62563e352b71089b5425ac8
betiiy-haile <betelhemhaile74@gmail.com>
2023-04-30 22:51:55 +0300

    Setup Github Context

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import Footer from "./components/layout/Footer";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
 import About from "./Pages/About";
+import { GithubProvider } from "./context/Github/GithubContext";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <NavBar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/notfound" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <NavBar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/notfound" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/components/users/UserResult.jsx
+++ src/components/users/UserResult.jsx
@@ -24,7 +24,7 @@ function UserResult() {
     } 
     if (!loading){
         return (
-        <div className="grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        <div className="grid gap-6 grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
             <UserItem key={user.id} user={user} />
         ))}
